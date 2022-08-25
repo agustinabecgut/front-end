367b0730a10ffcf6dd3d7fd66257ca945f53c58e
agustinabecgut <agustina.b.becerra@gmail.com>
2022-08-24 23:52:31 -0300

    solucionando problemas con el apartado experiencia y agregando estilos

--- src/app/components/experiencia/new-experiencia.component.ts
+++ src/app/components/experiencia/new-experiencia.component.ts
@@ -19,12 +19,15 @@ export class NewExperienciaComponent implements OnInit {
 
   onCreate(): void {
     const expe = new Experiencia(this.nombreE, this.descripcionE);
-    this.sExperiencia.save(expe).subscribe(data => {alert("Experiencia añadida");
-  this.router.navigate(['']);
-  }, err => {
-    alert("Falló");
-    this.router.navigate(['']);
-  })
+    this.sExperiencia.save(expe).subscribe(
+      data=>{
+        alert("Experiencia añadida");
+        this.router.navigate(['']);
+      }, err =>{
+        alert("Falló");
+        this.router.navigate(['']);
+      }
+    )
   }
 
 }
